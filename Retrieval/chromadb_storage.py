e51cf2c022278d6f6bc4f0212b00da61d9616a30
adegbite1548 <157822073+adegbite1548@users.noreply.github.com>
2026-08-31 09:13:01 +0100

    Add files via upload

--- Retrieval/chromadb_storage.py
+++ Retrieval/chromadb_storage.py
@@ -1,9 +1,10 @@
 import chromadb
-import ingest
-import chunk
+import Retrieval.ingest as ingest
+import Retrieval.chunk as chunk
 from pathlib import Path
 import pandas as pd
 from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
 
 def add_to_db(research_papers_path, collection):
 
@@ -15,18 +16,23 @@ def add_to_db(research_papers_path, collection):
 
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-    for research_paper in rp_paths:
+    for research_paper in tqdm(rp_paths, desc=f"Storing chunk embeddings for research papers in {collection.name}"):
 
         pdf_text = ingest.read_pdf(research_paper)
         
         pdf_chunks = chunk.chunk_text(pdf_text)
         chunk_ids = [f"{research_paper.stem}_chunk_{i}" for i in range(len(pdf_chunks))]
 
-        embeddings = model.encode(pdf_chunks)
+
+        try:
+
+            embeddings = model.encode(pdf_chunks)
         
+        except TypeError as e:
+            tqdm.write(f"[WARNING] {research_paper.name} : {e}")
         
-        pdf_metadata = metadata[metadata["ID"] == research_paper.stem].iloc[0, 1:].to_dict()
-        chunks_metadata = [pdf_metadata for chunks in pdf_chunks]
+        pdf_metadata = metadata[metadata["ID"] == research_paper.stem].iloc[0, :].to_dict()
+        chunks_metadata = [pdf_metadata for _ in pdf_chunks]
 
         collection.upsert(
             ids=chunk_ids,
@@ -40,9 +46,24 @@ def add_to_db(research_papers_path, collection):
 
 
 
-client = chromadb.PersistentClient(path="../chroma_db")
-client.delete_collection(name="research_paper_collection")
+client = chromadb.PersistentClient(path="chroma_db")
 
-collection = client.get_or_create_collection(name="research_paper_collection")
+try:
 
-add_to_db("Fetch_Papers/Research_Papers", collection)
+    client.delete_collection(name="research_paper_collection")
+except chromadb.errors.NotFoundError as e:
+    print(f"Collection could not be deleted as it does not exist. Creating Collection...")
+
+collection = client.get_or_create_collection(
+    name="research_paper_collection",
+    configuration={
+        "hnsw":{
+            "space": "cosine",
+            "ef_construction": 200,
+            "ef_search": 100,
+            "max_neighbors": 32
+        }
+    }
+)
+
+add_to_db("Retrieval/Fetch_Papers/Research_Papers", collection)
