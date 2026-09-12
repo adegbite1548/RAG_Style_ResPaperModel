1c2fa1f25c42eb72a8369d9afc728e8c66f4a52e
adegbite1548 <157822073+adegbite1548@users.noreply.github.com>
2026-09-12 01:07:50 +0100

    Add files via upload

--- Augmentation/Augment.py
+++ Augmentation/Augment.py
@@ -5,6 +5,6 @@ def augment_query_results(query_results):
     context = ""
     for i, (document, metadata) in enumerate(query_results.values()):
         single_line_document = document.replace("\n", " ")
-        context += f"TITLE {i+1} : {metadata['Title']}\n\nCONTEXT {i+1} : {single_line_document}\n\n\n"
+        context += f"TITLE  : {metadata['Title']}\n\nCONTEXT : {single_line_document}\n\n\n"
 
     return context
